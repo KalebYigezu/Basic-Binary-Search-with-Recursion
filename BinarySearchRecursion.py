6421457d6f10d9fac0dc7db81d36edf3a7fc0226
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-08-21 01:46:31 -0700

    Update BinarySearchRecursion.py

--- BinarySearchRecursion.py
+++ BinarySearchRecursion.py
@@ -1,1 +1,27 @@
+def binary_search(l, low, high, s):
+    if low <= high:
+        mid = (low + high) // 2
+        if l[mid] == s:
+            print(s, " is found at position", mid + 1)
+        elif l[mid] > s:
+            binary_search(l, low, mid - 1, s)
+        else:
+            binary_search(l, mid + 1, high, s)
+    else:
+        print(s, " is not found")
+
+
+l = []
+n = int(input("Enter a number of elements to be inserted: "))
+print("Enter ", n, " elements :")
+for i in range(n):
+    l.append(int(input()))
+l.sort()
+print("The sorted list is:", l)
+s = int(input("Enter a number to be searched for : "))
+
+low = 0
+high = len(l) - 1
+
+binary_search(l, low, high, s)
 
